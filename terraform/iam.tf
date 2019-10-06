43fa07774f372b4c29ce7444cd92c54fb73b73c8
twebber90 <twebber@tractorzoom.com>
2019-10-05 20:27:31 -0500

    chore: add s3 bucket replication

---
+++ terraform/iam.tf
@@ -0,0 +1,65 @@
+resource "aws_iam_role" "replication" {
+  name = "tf-iam-role-replication"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "replication" {
+  name = "tf-iam-role-policy-replication"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.site.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.site.arn}/*"
+      ]
+    },
+    {
+      "Action": [
+        "s3:ReplicateObject",
+        "s3:ReplicateDelete"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.site_replication.arn}/*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "replication" {
+  name       = "tf-iam-role-attachment-replication"
+  roles      = ["${aws_iam_role.replication.name}"]
+  policy_arn = "${aws_iam_policy.replication.arn}"
+}
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,10 @@
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "s3-log-bucket"
+  acl    = "log-delivery-write"
+
+  tags = "${local.common_tags}"
+}
+
 resource "aws_s3_bucket" "site" {
   bucket        = "${var.s3_bucket_name}"
   acl           = "private"
@@ -24,9 +31,45 @@ resource "aws_s3_bucket" "site" {
   ]
 }
 EOF
+
+  versioning {
+    enabled = true
+  }
+
+  replication_configuration {
+    role = "${aws_iam_role.replication.arn}"
+
+    rules {
+      id     = "sit_replication_rule"
+      prefix = "set_replication"
+      status = "Enabled"
+
+      destination {
+        bucket        = "${aws_s3_bucket.site_replication.arn}"
+        storage_class = "STANDARD"
+      }
+    }
+  }
+
   website {
     index_document = "index.html"
     error_document = "404.html"
+  }
+
+  logging {
+    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+    target_prefix = "log/"
+  }
+
+  tags = "${local.common_tags}"
+}
+
+resource "aws_s3_bucket" "site_replication" {
+  bucket = "${var.s3_bucket_name}-replication"
+  region = "us-central-1"
+
+  versioning {
+    enabled = true
   }
 
   tags = "${local.common_tags}"
